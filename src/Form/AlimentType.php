06788115a25847a4aaafe6a1b8d222b8c1d802de
AlbanDg <albanito.dugue@gmail.com>
2020-10-19 19:53:43 +0200

    création AlimentType

---
+++ src/Form/AlimentType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Aliment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AlimentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de l\'aliment'
+            ])
+            ->add('datePeremption', DateType::class, [
+                'label' => 'Date de péremption',
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Aliment::class,
+        ]);
+    }
+}
