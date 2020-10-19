787db4ab99c3f7f566bb3603e01b9d232de9f567
AlbanDg <albanito.dugue@gmail.com>
2020-10-19 19:37:13 +0200

    entity aliment ajouté

---
+++ src/Entity/Aliment.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AlimentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AlimentRepository::class)
+ */
+class Aliment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $proprietaire;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datePeremption;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProprietaire(): ?User
+    {
+        return $this->proprietaire;
+    }
+
+    public function setProprietaire(?User $proprietaire): self
+    {
+        $this->proprietaire = $proprietaire;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDatePeremption(): ?\DateTimeInterface
+    {
+        return $this->datePeremption;
+    }
+
+    public function setDatePeremption(\DateTimeInterface $datePeremption): self
+    {
+        $this->datePeremption = $datePeremption;
+
+        return $this;
+    }
+}
