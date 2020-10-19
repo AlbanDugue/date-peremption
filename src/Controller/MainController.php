9212f1cf9454f4a02d4715ee6ec46f2035cd8614
AlbanDg <albanito.dugue@gmail.com>
2020-10-19 20:30:38 +0200

    controller form aliment

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Aliment;
+use App\Form\AlimentType;
+use App\Repository\AlimentRepository;
+use App\Repository\CampusRepository;
+use App\Repository\EtatRepository;
+use App\Repository\LieuRepository;
+use App\Repository\UserRepository;
+use App\Repository\VilleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -10,10 +20,32 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(  Request $request,
+                            EntityManagerInterface $em,
+                            UserRepository $userRepository,
+                            AlimentRepository $alimentRepository
+                            )
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $proprietaire = $this->getUser();
+
+        $aliment = new Aliment();
+        $alimentForm = $this->createForm(AlimentType::class, $aliment);
+
+        $alimentForm->handleRequest($request);
+
+        if ($alimentForm->isSubmitted() && $alimentForm->isValid()) {
+
+            $aliment->setProprietaire($proprietaire);
+
+            $em->persist($aliment);
+            $em->flush();
+        }
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'alimentForm' => $alimentForm->createView()
         ]);
     }
 }
